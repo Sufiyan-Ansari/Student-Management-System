207a5b0d6da1dea5ee8138e282614f1be460cab4
Sufiyan Ansari <Sufiyan_Ansari@outlook.com>
2019-11-05 21:27:10 +0500

    Module Initialization

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+//logic tycoon
+
 const express = require('express');
 
 const app = express();
 
+app.set('view engine','ejs');
+
+app.set('views','views');
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'bower_components')));
+
 const port = process.env.PORT||3000;
+
 app.get('/',(req,res,next)=>{
-    res.send('Abcd');
+    res.render('Add-Student');
 });
 
 app.listen(port,()=>{
